73e08550b20bc989ccb0082d42a74b1613895c5d
Joshua Smith <joshsmith01@me.com>
2018-05-07 23:07:54 -0700

    Add some menu locations

--- functions.php
+++ functions.php
@@ -1,3 +1,28 @@
 <?php
 
 require get_template_directory() . '/includes/settings.php';
+
+
+$args = array(
+	'name'          => __( 'Primary', 'headless-wp' ),
+	'id'            => 'primary',    // ID should be LOWERCASE  ! ! !
+	'description'   => 'This sidebar will appear on all pages, unless explicitly overridden or not included.',
+	'class'         => '',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h2 class="widgettitle">',
+	'after_title'   => '</h2>'
+);
+register_sidebar( $args );
+
+$args2 = array(
+	'name'          => __( 'Secondary', 'headless-wp' ),
+	'id'            => 'secondary',    // ID should be LOWERCASE  ! ! !
+	'description'   => 'This sidebar will appear on all pages, unless explicitly overridden or not included.',
+	'class'         => '',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h2 class="widgettitle">',
+	'after_title'   => '</h2>'
+);
+register_sidebar( $args2 );
