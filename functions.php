8366ed9690d862d8e46233d8802c84093d702ba5
Joshua Smith <joshsmith01@me.com>
2018-05-13 22:09:46 -0700

    Add setup function that will setup many WP features in the theme
    post thumbnails
    translations

--- functions.php
+++ functions.php
@@ -26,3 +26,63 @@ $args2 = array(
 	'after_title'   => '</h2>'
 );
 register_sidebar( $args2 );
+
+if ( ! function_exists( 'headless-wp_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * Create your own headless-wp_setup() function to override in a child theme.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 */
+	function headless_wp_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed at WordPress.org. See: https://translate.wordpress.org/projects/wp-themes/headless-wp
+		 * If you're building a theme based on Twenty Sixteen, use a find and replace
+		 * to change 'headless-wp' to the name of your theme in all the template files
+		 */
+		load_theme_textdomain( 'headless-wp' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+		 */
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1200, 9999 );
+
+		/*
+		 * Enable support for Post Formats.
+		 *
+		 * See: https://codex.wordpress.org/Post_Formats
+		 */
+		add_theme_support( 'post-formats', array(
+			'aside',
+			'image',
+			'video',
+			'quote',
+			'link',
+			'gallery',
+			'status',
+			'audio',
+			'chat',
+		) );
+	}
+endif; // headless-wp_setup
+add_action( 'after_setup_theme', 'headless_wp_setup' );
